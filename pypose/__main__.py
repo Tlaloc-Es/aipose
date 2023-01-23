e26307ae834cca567ec6a57548396e77d10aba3d
Tlaloc-Es <dev@tlaloc-es.com>
2023-01-23 21:30:53 +0100

    feat(package): set up build package with a function and added poe the poet to add scripts

--- pypose/__main__.py
+++ pypose/__main__.py
@@ -1,14 +1,19 @@
 import os
 
-import cv2
-from numpy import ndarray
+import click
 
 from pypose.model import YoloV7Pose
 from pypose.webcam import process_webcam
 
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
-image: ndarray = cv2.imread("./person.jpg")
-model = YoloV7Pose("./yolov7-w6-pose.pt")
+@click.command()
+def webcam():
+    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
-process_webcam(model)
+    model = YoloV7Pose("./yolov7-w6-pose.pt")
+
+    process_webcam(model)
+
+
+if __name__ == "__main__":
+    webcam()
