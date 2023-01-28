ee78970291c65b9f24efd30d553ea3e3d7623d42
Tlaloc-Es <dev@tlaloc-es.com>
2023-01-28 20:03:51 +0100

    feat(frame_managers.py): added new managers

--- aipose/webcam/frame_managers.py
+++ aipose/webcam/frame_managers.py
@@ -4,16 +4,39 @@ import cv2
 import numpy as np
 from numpy import ndarray
 
-from aipose.model import Keypoints
+from aipose.model import Keypoints, YoloV7Pose
 from aipose.plot import plot
 
 
 class FrameManager:
+    stop = False
+
     def __init__(self):
         pass
 
     def on_frame(self, frame: ndarray) -> ndarray:
         return frame
+
+    def to_stop(self) -> bool:
+        return self.stop
+
+
+class FrameYoloV7(FrameManager):
+    def __init__(self):
+        self.model = YoloV7Pose()
+
+    def on_frame(self, frame: ndarray) -> ndarray:
+        prediction, image_tensor = self.model(frame)
+        proccessed_frame = self.on_predict(frame, prediction, image_tensor)
+        if proccessed_frame is None:
+            return frame
+        else:
+            return frame
+
+    def on_predict(
+        self, frame: ndarray, prediction: List[Keypoints], image_tensor: ndarray
+    ) -> None | ndarray:
+        raise NotImplementedError
 
 
 class FramePlot(FrameManager):
