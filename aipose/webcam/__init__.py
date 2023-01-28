c6ee0d277c9c1ce3c42ef5bc8f3cad336f9a5033
Tlaloc-Es <dev@tlaloc-es.com>
2023-01-28 20:03:25 +0100

    refactor(webcam.md): update webcam.md code and style

--- aipose/webcam/__init__.py
+++ aipose/webcam/__init__.py
@@ -1,21 +1,20 @@
 import cv2
-
+import torch
 from aipose.webcam.frame_managers import FrameManager
 
 
-def process_webcam(frame_manaer: FrameManager = FrameManager()):
+def process_webcam(frame_manager: FrameManager):
     capture = cv2.VideoCapture(0)
 
     while capture.isOpened():
-        import torch
-
         torch.cuda.empty_cache()
 
         ret, frame = capture.read()
-        frame = frame_manaer.on_frame(frame)
+        frame = frame_manager.on_frame(frame)
 
-        cv2.imshow("webCam", frame)
-        if cv2.waitKey(1) == ord("s"):
+        cv2.imshow("AIPOSE - WebCam - Press 's' to exit", frame)
+
+        if cv2.waitKey(1) == ord("s") or frame_manager.to_stop():
             break
 
     capture.release()
