a2a24939c42dfe48cd328d18ad0c7e2c2dab9152
Tlaloc-Es <dev@tlaloc-es.com>
2023-03-05 22:41:01 +0100

    docs(readthedocs): add template of readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = "Lumache"
-copyright = "2021, Graziella"
-author = "Graziella"
+project = "aipose"
+copyright = "2022, Taloc-Es"
+author = "Taloc-Es"
 
 release = "0.1"
 version = "0.1.0"
